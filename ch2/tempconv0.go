9e3ec658671af39f709a5d11acfd87795b864365
root1 <843355856@qq.com>
2018-09-02 17:04:56 +0800

    commit Fahrenheit Celsius example

--- ch2/tempconv0.go
+++ ch2/tempconv0.go
@@ -14,6 +14,29 @@ const (
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 func FtoC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
+func (c Celsius) String() string {
+	return fmt.Sprintf("%g°c", c)
+}
+
 func main() {
-	fmt.Printf("%.2f°C to %.2f°F", AbsoluteZeroC, CToF(AbsoluteZeroC))
+	fmt.Printf("%g\n", BoilingC-FreezingC)
+	boilingF := CToF(BoilingC)
+	fmt.Printf("%g\n", boilingF-CToF(FreezingC))
+	//fmt.Printf("%g\n", boilingF - FreezingC)
+
+	var c Celsius
+	var f Fahrenheit
+	fmt.Println(c == 0)
+	fmt.Println(f >= 0)
+	//fmt.Println(c == f) // complie error:  type mismatch
+	fmt.Println(c == Celsius(f))
+
+	fmt.Println("++++++++++++++++++++\n")
+	c = FtoC(212.0)
+	fmt.Println(c.String())
+	fmt.Printf("%v\n", c)
+	fmt.Printf("%s\n", c)
+	fmt.Println(c)
+	fmt.Printf("%g\n", c)
+	fmt.Println(float64(c))
 }
---
+++ ch2/tempconv0test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"the_go_programing_language/ch2/tempconv"
+)
+
+func main() {
+	fmt.Printf("Brrrr! %v\n", tempconv.AbsoluteZeroC)
+}
